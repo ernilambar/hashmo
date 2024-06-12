ee9e38071e13960c42aaaf3cfe69aa44a79aeff9
Nilambar Sharma <nilambar@outlook.com>
2024-06-12 15:50:15 +0545

    Fix defaults

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ program
   .usage('[options]')
   .description('Generate hash from current timestamp.')
   .addOption(new Option('-t, --type <type>', 'Output type').choices(['plaintext', 'php']).default('plaintext'))
-  .option('-d, --destination <dir>', 'Destination directory. Defaults to root directory.')
+  .option('-d, --destination <dir>', 'Destination directory. Defaults to root directory.', '')
   .option('-o, --output <filename>', 'Output file name with extension', 'hash.txt')
   .action((options) => {
     generateHash(options)
   })
-  .version('1.0.0', '-v, --version', 'Output the current version.')
+  .version('1.0.1', '-v, --version', 'Output the current version.')
   .helpOption('-h, --help', 'Display help for command.')
 
 program.parse()
