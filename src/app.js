1fc987d56618e9754bb4849454b3a83a2cd08934
Nilambar Sharma <nilambar@outlook.com>
2024-06-14 16:25:57 +0545

    Add raw option

--- src/app.js
+++ src/app.js
@@ -2,13 +2,15 @@ import path from 'path'
 import { getHashString, getFileContent, createDir, handleHashFile } from './utils.js'
 
 const generateHash = (flags) => {
+  const isRaw = !!(flags.raw)
+
   const { type, destination, output } = flags
 
   const fullDestination = path.resolve(destination)
 
   createDir(fullDestination)
 
-  handleHashFile(path.join(fullDestination, output), getFileContent(getHashString(), type))
+  handleHashFile(path.join(fullDestination, output), getFileContent(getHashString(isRaw), type))
 }
 
 export { generateHash }
